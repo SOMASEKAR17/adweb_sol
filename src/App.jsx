c00561ef446f375fbfabe92d483fadf22e69a955
SOMASEKAR17 <somasekarnaidu79@gmail.com>
2025-06-02 16:11:58 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,12 @@ const LandingPage = () => {
           </a>
           <nav className="navbar" data-navbar>
             <ul className="navbarlist">
-              <li><a href="#" className="navbar-link">How it works</a></li>
+              <li><a href="#process-label" className="navbar-link">How it works</a></li>
               <li><a href="#work" className="navbar-link">services</a></li>
-              <li><a href="#" className="navbar-link">About us</a></li>
+              <li><a href="#contact" className="navbar-link">Contact Us</a></li>
             </ul>
           </nav>
-          <a href="#" className="btn btn-secondary">Try it Now</a>
+          <a href="#contact" className="btn btn-secondary">Try it Now</a>
           <button className="nav-open-button" aria-label="toggle menu" data-nav-toggle-btn>
             <ion-icon name="menu-outline" aria-hidden="true" class="menu-icon"></ion-icon>
             <ion-icon name="close-outline" aria-hidden="true" class="close-icon"></ion-icon>
@@ -28,12 +28,12 @@ const LandingPage = () => {
 
       <main>
         <article>
-          <section className="section hero">
-            <div className="container">
+          <section  className="section hero">
+            <div id='contact' className="container">
               <div className="hero-content">
                 <h1 className="h1 title hero-title">We Take Care of Your Brand</h1>
                 <p className="hero-text">
-                  Use this section to describe your company and the products you offer. You could share your company’s story and details about why you are in business.
+                 AdWeb sol helps brands grow with high-performing ad campaigns and custom landing pages designed to convert. We handle everything from eye-catching ads to optimized pages, ensuring your audience clicks—and converts. Perfect for startups and growing businesses ready to scale.
                 </p>
                 <form action="https://formspree.io/f/xyzwrqja" method="POST" className="input-wrapper">
                   <input type="email" name="email-address" placeholder="Enter Your Email" className="input-field" />
@@ -53,18 +53,18 @@ const LandingPage = () => {
             <div className="container">
               <h2 className="h2 title section-title" id="process-label">How It Works</h2>
               <p className="section-text">
-                Use this section to describe your company and the products you offer. You could share your company’s story and details about why you are in business.
+                From idea to impact — here’s how we bring your brand to life with powerful ad campaigns and landing pages that convert.
               </p>
               <div className="wrapper">
                 <figure className="process-banner">
                   <img src="process-banner.png" width="734" height="854" loading="lazy" alt="process banner" className="w-100" />
                 </figure>
                 <ul className="grid-list">
-                  {["Ideation", "Budget", "Performance", "Speed"].map((title, i) => (
-                    <li className="process-item" key={title}>
+                  {[{"title": "Ideation","disc":"We start by understanding your brand, audience, and goals. Then, we brainstorm compelling messaging, visuals, and funnel strategies to create a campaign with a purpose."}, {"title": "Budget","disc":"We work within your budget to maximize reach and ROI. Whether you're scaling or starting out, we tailor our media spend and strategy to get you results."}, {"title": "Performance","disc":"We don’t just launch and leave — we track, analyze, and optimize. Our team continuously tests and refines campaigns and landing pages to boost performance."}, {"title": "Speed","disc":"Time is money. We move fast, delivering campaigns and landing pages with quick turnarounds — without compromising on quality."}].map((item, i) => (
+                    <li className="process-item" key={item.title}>
                       <img src={` process-icon-${i + 1}.png`} width="60" height="60" loading="lazy" alt="" className="item-icon" />
-                      <h3 className="h3 title item-title">{title}</h3>
-                      <p className="item-text">Use this section to describe your company and the products you offer. You could share your company’s</p>
+                      <h3 className="h3 title item-title">{item.title}</h3>
+                      <p className="item-text">{item.disc}</p>
                     </li>
                   ))}
                 </ul>
@@ -74,24 +74,37 @@ const LandingPage = () => {
 
           <section id="work" className="section works text-center" aria-label="works">
             <div className="container">
-              <h2 className="h2 title section-title">Our Work</h2>
+              <h2 className="h2 title section-title">Services We Provide</h2>
               <p className="section-text">
-                Use this section to describe your company and the products you offer. You could share your company’s story and details about why you are in business.
+                We blend creativity, strategy, and technology to craft digital experiences that drive real-world results. Here’s a glimpse into what we do best:
               </p>
               <ul className="grid-list">
-                {["Product Design", "Digital marketing", "Video Editing"].map((title, i) => (
-                  <li key={title}>
+                {[{"title": "Landing Page"}, {"title": "Digital marketing"}, {"title": "Video Editing"}].map((item, i) => (
+                  <li key={item.title}>
                     <div className="work-card">
                       <figure className="card-banner img-holder" style={{ '--width': 580, '--height': 620 }}>
-                        <img src={` work-${i + 1}.jpg`} width="580" height="620" loading="lazy" alt={title} className="img-cover" />
+                        <img src={` work-${i + 1}.${(i === 0) ? 'png' : 'jpg'}`} width="580" height="620" loading="lazy" alt={item.title} className="img-cover" />
                       </figure>
-                      <h3 className="h3 card-title">{title}</h3>
+                      <h3 className="h3 card-title">{item.title}</h3>
                       <p className="card-text">Use this section to describe your work</p>
                     </div>
                   </li>
                 ))}
               </ul>
-              <a href="#" className="btn btn-primary">See More</a>
+              <section class="conversion-section">
+                  <h2 class="conversion-title">Conversion Benchmark</h2>
+
+                  <div class="conversion-wrapper">
+                    <p class="conversion-text">We help companies achieve up to</p>
+                    <div class="conversion-circle">80%</div>
+                    <p class="conversion-text">conversion rate</p>
+                  </div>
+
+                  <p class="conversion-note">
+                    We’re dedicated to making every rupee you spend on advertising work harder, delivering up to 
+                    10X the results, and helping your business thrive.
+                  </p>
+              </section>
             </div>
           </section>
         </article>
@@ -118,7 +131,7 @@ const LandingPage = () => {
             </div>
             <ul className="footer-list">
               {["Work With Us", "Advertise With Us", "Support Us", "Business Advices", "Private Coaching", "Our Work", "Our Commitment", "Our Team", "About Us", "FAQs", "Report a Bug"].map(link => (
-                <li key={link}><a href="#" className="footer-link">{link}</a></li>
+                <li key={link} className="footer-link">{link}</li>
               ))}
             </ul>
           </div>
@@ -130,8 +143,8 @@ const LandingPage = () => {
             </p>
           </div>
           <ul className="footer-bottom-list">
-            <li><a href="#" className="footer-link">Terms of use</a></li>
-            <li><a href="#" className="footer-link">Privacy policy</a></li>
+            <li className="footer-link">Terms of use</li>
+            <li className="footer-link">Privacy policy</li>
           </ul>
         </div>
       </footer>
